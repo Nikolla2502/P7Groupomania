bc445b802f5a2c9aa8ce8fd54fae85a6c69ee2eb
Nikolla2502 <nikolla@wanadoo.fr>
2021-11-17 11:40:36 +0100

    frontend vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+  <contenu></contenu>
+  </div>
+</template>
+
+<script>
+
+import Contenu from './components/contenu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'contenu':Contenu
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/contenu.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>{{ txt }}</div>
+</template>
+
+
+<script>
+export default {
+    data(){                  //  equivalent à data: function()
+        return {
+            txt : 'Je suis le contenu'
+        }
+    }
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
