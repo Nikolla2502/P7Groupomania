3b28efdc362f3d6ae1af317dcd7f2b505fb43a31
Nikolla2502 <nikolla@wanadoo.fr>
2021-11-18 01:59:53 +0100

    ajout liste.vue

--- frontend/src/components/contenu.vue
+++ frontend/src/components/contenu.vue
@@ -1,14 +1,22 @@
 <template>
-    <div>{{ txt }}</div>
+    <div>
+        <h1>{{ txt }}</h1>
+        <liste></liste>
+    </div>
 </template>
 
 
 <script>
+import Liste from './liste.vue'
 export default {
+    name: 'Contenu',
     data(){                  //  equivalent à data: function()
         return {
-            txt : 'Je suis le contenu'
+            txt : 'Bienvenue sur votre reseau d\'entreprise'
         }
+    },
+    components: {
+        'liste': Liste
     }
 }
 
